3b40f4a8cf1c502b2c66635cac550e2b783b95e7
ahn-sj <jaesa5221@gmail.com>
2025-06-23 11:41:51 +0900

    Initial Project

---
+++ src/main/java/tally/transfer/ScheduledTransferSystemApplication.java
@@ -0,0 +1,13 @@
+package tally.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledTransferSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledTransferSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tally/transfer/ScheduledTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tally.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledTransferSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
